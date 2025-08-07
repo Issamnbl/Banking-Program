59c339582a185827897c6fa215f2a6fdc10ebf75
Issa Manibale <issamanibale@gmail.com>
2025-08-07 19:11:34 +0800

    commit init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,37 +2,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // WEIGHT CONVERSION PROGRAM
-
         Scanner scanner = new Scanner(System.in);
 
-        double weight;
-        double newWeight;
-        int choice;
+        double temp;
+        double newTemp;
+        String unit;
 
-        System.out.println("Weight Conversion Program");
-        System.out.println("1: Convert lbs to kgs");
-        System.out.println("2: COnvert kgs to lbs");
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
 
-        System.out.print("Choose an option: ");
-        choice = scanner.nextInt();
+        System.out.print("Convert to Celsius or Fahrenheit (C or F): ");
+        unit = scanner.next().toUpperCase();
 
-        if (choice == 1) {
-            System.out.print("Enter the weight in lbs: ");
-            weight = scanner.nextDouble();
-            newWeight = weight * 0.453592;
-            System.out.printf("The new weight in kgs is : %.2f", newWeight);
-        } else if (choice == 2) {
-            System.out.print("Enter the weight in kgs: ");
-            weight = scanner.nextDouble();
-            newWeight = weight * 2.20462;
-            System.out.printf("The new weight in lbs is : %.2f", newWeight);
-        }
-         else{
-            System.out.println("That was not a valid choice");
-        }
+        // (condition) ? true : false
 
+      newTemp =  (unit.equals("C")) ? (temp -32) * 5 / 9 : (temp * 5 / 9) + 32;
+
+        System.out.printf("%.1f°%s", newTemp, unit);
         scanner.close();
+
+
+
+
     }
 }
 

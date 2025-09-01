f69f49da810e19220df10fc86d36d49e0d52cd81
Issa Manibale <issamanibale@gmail.com>
2025-09-01 13:02:34 +0800

    commit init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,90 @@
-import java.util.Random;
+import java.sql.SQLOutput;
 import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-      //NUMBER GUESSING GAME
+        //BANKING PROGRAM
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
 
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
+        while (isRunning) {
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
 
-        int guess;
-        int attempts = 0;
-        int min = 1;
-        int max = 100;
-        int randomNumber = random.nextInt(min, max + 1);
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
 
-        System.out.println("Number Guessing Game");
-        System.out.printf("Guess the number between %d-%d\n", min, max);
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance = balance + deposit();
+                case 3 -> balance = balance - withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
 
-        do{
-            System.out.print("Enter a guess: ");
-            guess = scanner.nextInt();
-            attempts++;
-
-            if(guess < randomNumber){
-                System.out.println("TOO LOW! Try again");
             }
-            else if(guess > randomNumber){
-                System.out.println("TOO HIGH! Try again");
-            }
-            else{
-                System.out.println("CORRECT! The number was " + randomNumber);
-                System.out.println("# of attempts: " + attempts);
-            }
+        }
 
-        }while(guess != randomNumber);
+        System.out.println("***************************");
+        System.out.println("THANK YOU! HAVE A NICE DAY!");
+        System.out.println("***************************");
 
         scanner.close();
 
 
+    }
 
+    static void showBalance(double balance) {
+        System.out.println("***************");
+        System.out.printf("$%.2f\n", balance);
+    }
 
+    static double deposit() {
 
+        double amount;
 
-      
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+        if (amount < 0) {
+            System.out.println("Amount can't be negative");
+            return 0;
+        } else {
+            return amount;
+        }
+
+    }
+
+    static double withdraw(double balance) {
+        double amount;
+
+        System.out.print("Enter amount to be withdrawn: ");
+        amount = scanner.nextDouble();
+
+        if (amount > balance) {
+            System.out.println("INSUFFICIENT FUNDS");
+            return 0;
+        } else if (amount < 0) {
+            System.out.println("Amount can't be negative");
+            return 0;
+        } else {
+            return amount;
+        }
     }
 }
+
+
+
+
 
 
 
